e6610fd1faaeeb76461b57ea1da500a541f4ca91
Titouan Hinschberger <titouan.hinschberger@univ-tours.fr>
2025-05-03 16:15:54 +0200

    expand and collapse fieldset

---
+++ data/rarities.js
@@ -0,0 +1,10 @@
+let rarities = new Map([
+    ["Rare", new Map([ ["price","29 <span class='gem'></span> or 1000 <span class='bling'></span>"], ["color","#00e300"], ["icon","https://static.wikia.nocookie.net/brawlstars/images/a/a2/Catalogue-Rare.png"], ])],
+    ["Super Rare", new Map([ ["price","79 <span class='gem'></span> or 2750 <span class='bling'></span>"], ["color","#14a4ff"], ["icon","https://static.wikia.nocookie.net/brawlstars/images/3/3b/Catalogue-Super_Rare.png"], ])],
+    ["Epic", new Map([ ["price","149 <span class='gem'></span> or 5000 <span class='bling'></span>"], ["color","#b116ec"], ["icon","https://static.wikia.nocookie.net/brawlstars/images/a/aa/Catalogue-Epic.png"], ])],
+    ["Mythic", new Map([ ["price","199 <span class='gem'></span>"], ["color","#f04141"], ["icon","https://static.wikia.nocookie.net/brawlstars/images/3/33/Catalogue-Mythic.png"], ])],
+    ["Legendary", new Map([ ["price","299 <span class='gem'></span>"], ["color","#fdf01f"], ["icon","https://static.wikia.nocookie.net/brawlstars/images/1/1d/Catalogue-Legendary.png"], ])],
+    ["Hypercharge", new Map([ ["price","399 <span class='gem'></span>"], ["color","#f800ff"], ["icon","https://static.wikia.nocookie.net/brawlstars/images/6/6b/Catalogue-Hypercharge.png"], ])],
+    ["True Silver", new Map([ ["price","10000 <span class='coin'></span>"], ["color","#829ee2"], ["icon","https://static.wikia.nocookie.net/brawlstars/images/e/e6/Campaign_Icon-True_Silver.png"], ])],
+    ["True Gold", new Map([ ["price","25000 <span class='coin'></span> and True Silver skin"], ["color","#fca405"], ["icon","https://static.wikia.nocookie.net/brawlstars/images/5/52/Campaign_Icon-True_Gold.png"], ])],
+]);
